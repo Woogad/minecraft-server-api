f7ee65ad81c127f1794d903ed4cef438f9502a8d
teerasak <vgthailand@hotmail.com>
2025-04-19 15:50:20 +0700

    minecraft server api

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Woogad/minecraft-server-api
 
 go 1.24.2
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/mcstatus-io/mcutil/v4 v4.0.1
+)
--- main.go
+++ main.go
@@ -1,21 +1,69 @@
-import (
-    "context"
-    "fmt"
-    "time"
+package main
 
-    "github.com/mcstatus-io/mcutil/v4/status"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/mcstatus-io/mcutil/v4/status"
 )
 
+type ServerStatus struct {
+	Online       string   `json:"online"`
+	Host         string   `json:"host"`
+	Version      string   `json:"version"`
+	MaxPlayers   int      `json:"max_players"`
+	PlayerOnline int      `json:"player_count"`
+	PlayerList   []string `json:"player_list"`
+}
+
 func main() {
-    ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    defer cancel()
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r) // or just do nothing
+	})
+	http.HandleFunc("/", serverStatusHandler)
 
-    response, err := status.Modern(ctx, "demo.mcstatus.io")
+	fmt.Println("Server is running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-    if err != nil {
-        panic(err)
-    }
+func serverStatusHandler(w http.ResponseWriter, r *http.Request) {
+	address := os.Getenv("ADDRESS")
+	response := ServerStatus{
+		Host: address,
+	}
 
-    fmt.Println(response)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	resp, err := status.Modern(ctx, address, 25565)
+	if err != nil {
+		response.Online = "No"
+		fmt.Println("Failed to get server status: " + err.Error())
+	} else {
+
+		if len(resp.Players.Sample) > 0 {
+			for _, player := range resp.Players.Sample {
+				response.PlayerList = append(response.PlayerList, player.Name.Raw)
+			}
+		}
+
+		response.Online = "Yes"
+		response.PlayerOnline = int(*resp.Players.Online)
+		response.MaxPlayers = int(*resp.Players.Max)
+		response.Version = resp.Version.Name.Raw
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
